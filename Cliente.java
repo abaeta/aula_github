003b830e40d715d9ecdf9c55a73d33147394c880
Carlos Eduardo <carlos.bastos@vtex.com.br>
2021-05-08 20:01:07 -0300

    Add cadastrarCliente method #1

--- Cliente.java
+++ Cliente.java
@@ -2,6 +2,12 @@ public class Cliente {
     private String nome;
     private int cpf;
 
+    public Cliente(String nome, int cpf)
+    {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
     public void setNome(String nome)
     {
         this.nome = nome;
