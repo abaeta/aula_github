7ff5b81b25494bdae18364c8a4ed6bab8e91e31c
Carlos Eduardo <carlos.bastos@vtex.com.br>
2021-05-08 19:30:14 -0300

    Add Cliente getters and setters class #1

--- Cliente.java
+++ Cliente.java
@@ -1,4 +1,24 @@
 public class Cliente {
-    String nome;
-    int cpf;
+    private String nome;
+    private int cpf;
+
+    public void setNome(String nome)
+    {
+        this.nome = nome;
+    }
+
+    public String getNome()
+    {
+        return nome;
+    }
+
+    public void setCpf(int cpf)
+    {
+        this.cpf = cpf;
+    }
+
+    public int getCpf()
+    {
+        return cpf;
+    }
 }
