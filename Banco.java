04d8919d59f39a88a239e7cf33214934fed8916a
Pedro Antonio <pedroantonio.fernandes.dev@gmail.com>
2021-05-08 21:11:44 -0300

    Visualizar Saldo #5 (#12)
    * Add Cliente class #1
    
    * Add Cliente getters and setters class #1
    
    * Add cadastrarCliente method #1
    
    * Visualizar Saldo #5
    
    Classe Conta, método get_saldo em Conta e método get_saldo em Banco
    
    * Correções #5
    
    Correções do último pull request da issue #5
    
    * Correção double #5
    
    Correção do tipo da variável saldo de conta
    
    Co-authored-by: Carlos Eduardo <carlos.bastos@vtex.com.br>

--- Banco.java
+++ Banco.java
@@ -2,8 +2,9 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Banco {
+    private ArrayList<Conta> contas = new ArrayList<Conta>();
     private ArrayList<Cliente> clientes = new ArrayList<Cliente>();
-
+  
     //returns -1 if not found the client or returns the index of the clientes array if found
     public int findCliente(int cpf)
     {
@@ -36,6 +37,32 @@ public class Banco {
         clientes.add(new Cliente(nome, cpf));
 
         System.out.println("Cliente cadastrado com sucesso!");
+
+        sc.close();
+    }
+
+    public int findConta(int numero){
+        for(int i = 0; i < contas.size(); i++){
+            if(contas.get(i).getNumero() == numero)
+                return i;
+        }
+        return -1;
+    }
+
+    public void visualizarSaldo(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println(" ==== Visulizar saldo ==== \n");
+        
+        int index;
+        do
+        {
+            System.out.print("Digite seu numero da conta: ");
+            int numero = sc.nextInt();
+            index = findConta(numero);
+        }
+        while(index == -1);
+        
+        System.out.println("Saldo: " + contas.get(index).getSaldo());
 
         sc.close();
     }
---
+++ Conta.java
@@ -0,0 +1,12 @@
+public class Conta{
+    private int numero;
+    private double saldo;
+
+    public double getSaldo(){
+        return saldo;
+    }
+
+    public int getNumero(){
+        return numero;
+    }
+}
