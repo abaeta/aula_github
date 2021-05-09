4e92a2c2e8f42f8bb7ee2530d950005a6ea61ccb
Pedro Antonio <p.antonio.f.s@gmail.com>
2021-05-08 21:06:25 -0300

    Correção double #5
    Correção do tipo da variável saldo de conta

--- Conta.java
+++ Conta.java
@@ -1,8 +1,8 @@
 public class Conta{
     private int numero;
-    private int saldo;
+    private double saldo;
 
-    public int getSaldo(){
+    public double getSaldo(){
         return saldo;
     }
 
