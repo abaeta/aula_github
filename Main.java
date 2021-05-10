4d93d4410a3769c4b67eaae068ca3286b3c27893
Carlos Eduardo <carloseduardo@ifpi.edu.br>
2021-05-09 21:30:34 -0300

    Add Main Menu (#16)
    * Don't close scanner #8
    
    * Add main menu #8
    
    * Refactor code #8

--- Main.java
+++ Main.java
@@ -3,8 +3,45 @@ import java.util.Arrays;
 public class Main {
 
 	public static void main(String[] args) {
-		Menu mainMenu =  new Menu("Menu Principal", Arrays.asList("Conta", "Cliente", "Operacoes"));
-		System.out.println(mainMenu.getSelection() + "foi selecionada");
+		int op = 0;
+		Banco banco = new Banco();
+
+		do
+		{
+			Menu mainMenu =  new Menu("Menu Principal", Arrays.asList("Cadastrar Cliente", "Abrir Conta", "Remover Conta" , "Realizar Saque", "Realizar Depósito", "Visualizar Saldo", "Realizar Transferência"));
+			op = mainMenu.getSelection();
+
+			if (op == 1)
+			{
+				banco.cadastrarCliente();
+			}
+			else if (op == 2)
+			{
+				//TODO cadastrar conta
+			}
+			else if (op == 3)
+			{
+				//TODO remover conta
+			}
+			else if (op == 4)
+			{
+				banco.saque();
+			}
+			else if (op == 5)
+			{
+				banco.deposito();
+			}
+			else if (op == 6)
+			{
+				banco.visualizarSaldo();
+			}
+			else if(op == 7)
+			{
+				banco.transferencia();
+			}
+		}
+		while (op != 0);
+
 		System.out.println("Fim");
 	}
 
